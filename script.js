591493b9bbdf5ddc542623087c2aa7e5de255bc4
Богдан Меньшиков <menshikov93@gmail.com>
2019-09-14 13:50:09 +0300

    123

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(function () {
     var http = new XMLHttpRequest(), f = this;
     var th = $(this);
     evt.preventDefault();
-    http.open("POST", "contact.php", true);
+    http.open("POST", "/contact.php", true);
     http.onreadystatechange = function () {
       if (http.readyState == 4 && http.status == 200) {
         alert(http.responseText);
