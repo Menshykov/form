b39fab9578cdaeab18c430a16eb2c7377ca9356f
Menshykovkh <menshikov93@gmail.com>
2019-09-14 15:45:23 +0300

    fix

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 $(function () {
-  document.getElementById('ajax-contact-form').addEventListener('submit', function (evt) {
+  document.getElementById('ajax-contact-form').addEventListener('submit', function (e) {
     var http = new XMLHttpRequest(), f = this;
     var th = $(this);
-    evt.preventDefault();
-    http.open("POST", "contact.php", true);
-    http.onreadystatechange = function () {
-      if (http.readyState == 4 && http.status == 200) {
-        alert(http.responseText);
-        th.trigger("reset");
+    e.preventDefault();
+    var file = document.getElementById("file");
+
+    if (file.value !== ''){
+      http.open("POST", "contact.php", true);
+      http.onreadystatechange = function () {
+        if (http.readyState == 4 && http.status == 200) {
+          alert(http.responseText);
+          th.trigger("reset");
+        }
       }
+      http.onerror = function () {
+        alert('Ошибка, попробуйте еще раз');
+      }
+      http.send(new FormData(f));
+    } else {
+      alert('Выберите файл для загрузки');
     }
-    http.onerror = function () {
-      alert('Ошибка, попробуйте еще раз');
-    }
-    http.send(new FormData(f));
+    
+    
   }, false);
 
 });
