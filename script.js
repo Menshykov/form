930547a96ac5401dfbcf785c19d91aecea2f9ae5
Богдан Меньшиков <menshikov93@gmail.com>
2019-09-14 13:45:45 +0300

    changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(function() {
-  document.getElementById('ajax-contact-form').addEventListener('submit', function(evt){
+$(function () {
+  document.getElementById('ajax-contact-form').addEventListener('submit', function (evt) {
     var http = new XMLHttpRequest(), f = this;
     var th = $(this);
     evt.preventDefault();
     http.open("POST", "contact.php", true);
-    http.onreadystatechange = function() {
+    http.onreadystatechange = function () {
       if (http.readyState == 4 && http.status == 200) {
         alert(http.responseText);
         if (http.responseText.indexOf(f.nameFF.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (nameFF)
@@ -12,7 +12,7 @@ $(function() {
         }
       }
     }
-    http.onerror = function() {
+    http.onerror = function () {
       alert('Ошибка, попробуйте еще раз');
     }
     http.send(new FormData(f));
